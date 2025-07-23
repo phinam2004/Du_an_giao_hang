3bd552ea036d9bf104980085534c172a0cc2d415
thamlang112 <langtham2k4@gmail.com>
2025-07-23 19:10:14 +0700

    pn
    pn

---
+++ Delivery man app changes from V11.4 to V11.5/README.md
@@ -0,0 +1,21 @@
+# resturant_delivery_boy
+
+A new Flutter application.
+
+## Getting Started
+## Flutter SDK 3.32.5
+## User App Version 11.5
+## Deliveryman App Version 11.5
+## Table App Version 2.1
+## Kitchen App Version 2.1
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ User app changes from V11.4 to V11.5/lib/features/cart/widgets/checkout_button_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_restaurant/common/widgets/custom_button_widget.dart';
+import 'package:flutter_restaurant/features/auth/providers/auth_provider.dart';
+import 'package:flutter_restaurant/features/cart/widgets/free_delivery_progressbar_widget.dart';
+import 'package:flutter_restaurant/features/cart/widgets/guest_checkout_widget.dart';
+import 'package:flutter_restaurant/features/checkout/providers/checkout_provider.dart';
+import 'package:flutter_restaurant/features/coupon/providers/coupon_provider.dart';
+import 'package:flutter_restaurant/features/splash/providers/splash_provider.dart';
+import 'package:flutter_restaurant/helper/custom_snackbar_helper.dart';
+import 'package:flutter_restaurant/helper/price_converter_helper.dart';
+import 'package:flutter_restaurant/helper/responsive_helper.dart';
+import 'package:flutter_restaurant/helper/router_helper.dart';
+import 'package:flutter_restaurant/localization/language_constrants.dart';
+import 'package:flutter_restaurant/utill/dimensions.dart';
+import 'package:flutter_restaurant/utill/images.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class CheckOutButtonWidget extends StatelessWidget {
+  const CheckOutButtonWidget({
+    super.key,
+    required this.orderAmount,
+    required this.totalWithoutDeliveryFee,
+    required this.isFreeDelivery,
+  });
+
+  final double orderAmount;
+  final double totalWithoutDeliveryFee;
+  final bool isFreeDelivery;
+
+  @override
+  Widget build(BuildContext context) {
+    final splashProvider = Provider.of<SplashProvider>(context, listen: false);
+    final AuthProvider authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final CouponProvider couponProvider = Provider.of<CouponProvider>(context, listen: false);
+    final CheckoutProvider checkoutProvider = Provider.of<CheckoutProvider>(context, listen: false);
+
+    return ((splashProvider.configModel?.selfPickup ?? false) || (splashProvider.configModel?.homeDelivery ?? false)) ? Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(Dimensions.radiusDefault),
+          topRight: Radius.circular(Dimensions.radiusDefault),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).shadowColor,
+            blurRadius: 5,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+
+          FreeDeliveryProgressBarWidget(subTotal: totalWithoutDeliveryFee),
+
+          Container(
+            width: Dimensions.webScreenWidth,
+            padding: const EdgeInsets.all(Dimensions.paddingSizeSmall),
+            child: CustomButtonWidget(btnTxt: getTranslated('proceed_to_checkout', context), onTap: () {
+              if(authProvider.isLoggedIn()){
+                if(orderAmount < (splashProvider.configModel?.minimumOrderValue ?? 0)) {
+                  showCustomSnackBarHelper('${getTranslated('minimum_order_is', context)} ${PriceConverterHelper.convertPrice(splashProvider.configModel!
+                      .minimumOrderValue)}, ${getTranslated('you_have', context)} ${PriceConverterHelper.convertPrice(orderAmount)} ${getTranslated('in_your_cart_please_add_more', context)}');
+                } else {
+                  RouterHelper.getCheckoutRoute(
+                    totalWithoutDeliveryFee, 'cart',
+                    couponProvider.code, checkoutProvider.isCutlerySelected,
+                      isFreeDelivery ? "free_delivery" : ""
+                  );
+                }
+              } else{
+                ResponsiveHelper.showDialogOrBottomSheet(context, GuestCheckoutWidget(onGuestTap: () {
+                  if(orderAmount < splashProvider.configModel!.minimumOrderValue!) {
+                    showCustomSnackBarHelper('${getTranslated('minimum_order_is', context)} ${PriceConverterHelper.convertPrice(splashProvider.configModel!
+                        .minimumOrderValue)}, ${getTranslated('you_have', context)} ${PriceConverterHelper.convertPrice(orderAmount)} ${getTranslated('in_your_cart_please_add_more', context)}');
+                  } else {
+                    context.pop();
+
+                    RouterHelper.getCheckoutRoute(
+                      totalWithoutDeliveryFee, 'cart',
+                      couponProvider.code,
+                      checkoutProvider.isCutlerySelected,
+                      isFreeDelivery ? "free_delivery" : ""
+                    );
+                  }
+                }));
+              }
+            }),
+          ),
+        ],
+      ),
+    ) : const SizedBox();
+  }
+}
